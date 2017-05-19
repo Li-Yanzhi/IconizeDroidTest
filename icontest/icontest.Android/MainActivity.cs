7857cc9c7f9968cb6f81ed0becab626fdc1e0219
Yanzhi Li <yanzhil@microsoft.com>
2017-05-20 00:44:01 +0800

    添加项目文件。

---
+++ icontest/icontest.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using FormsPlugin.Iconize.Droid;
+
+namespace icontest.Droid
+{
+    [Activity(Label = "icontest", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            Plugin.Iconize.Iconize.With(new Plugin.Iconize.Fonts.FontAwesomeModule());
+            IconControls.Init(Resource.Id.toolbar, Resource.Id.sliding_tabs);
+
+            LoadApplication(new App());
+        }
+    }
+}
+
